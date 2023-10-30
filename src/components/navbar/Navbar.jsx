3a6fb9fe3f393275bdedd4e48ceef18bf483f1e9
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-10-30 11:06:33 -0300

    Projeto filmes
    término

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import { useState } from "react";
+
+import './Navbar.css'
+
+export default function Navbar() {
+
+  const [search, setSearch] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    if(!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch("")
+  }
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to={"/"}><BiCameraMovie /> CinemaLib</Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="" id="" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)} value={search}/>
+        <button type="submit"><BiSearchAlt2 /></button>
+      </form>
+    </nav>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App.jsx'
+import Home from './pages/home/Home.jsx'
+import Movie from './pages/movie/Movie.jsx'
+import Search from './pages/search/Search.jsx'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='movie/:id' element={<Movie />} />
+          <Route exact path='search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
