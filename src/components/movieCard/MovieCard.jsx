7ab7359e73332cb41a61052db90b83a943627954
Filipe Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2024-01-02 10:41:36 -0300

    modificação da layout do movie detalhes

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -3,12 +3,12 @@ import { FaStar } from 'react-icons/fa'
 
 const imageUrl = import.meta.env.VITE_IMG
 
-export default function MovieCard({movie, showLink = true}){
+export default function MovieCard({movie, showLink = true, showMovieTitle = true, showMovieVoteAverage=true}){
     return(
         <div className='movie-card'>
             <img src={`${imageUrl}/${movie.poster_path}`} alt={movie.title} />
-            <h2>{movie.title}</h2>
-            <p><FaStar /> {movie.vote_average.toFixed(2)} / 10</p>
+            {showMovieTitle && <h2>{movie.title}</h2>}
+            {showMovieVoteAverage && <p><FaStar /> {movie.vote_average.toFixed(2)} / 10</p>}
             {showLink && <Link to={`/movie/${movie.id}`} >Detalhes</Link>}
         </div>
     )
--- src/pages/movie/Movie.jsx
+++ src/pages/movie/Movie.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { BsGraphUp, BsWallet2, BsHourglassSplit } from 'react-icons/bs'
+import { FaStar } from "react-icons/fa"
 
 import MovieCard from "../../components/movieCard/MovieCard"
 
@@ -9,17 +10,17 @@ import './Movie.css'
 const moviesURL = import.meta.env.VITE_API;
 const apikey = import.meta.env.VITE_API_KEY;
 
-export default function Movie(){
-    const {id} = useParams()
+export default function Movie() {
+    const { id } = useParams()
 
     const [movie, setMovie] = useState(null)
 
-    const getMovie = async(url) => {
+    const getMovie = async (url) => {
         const res = await fetch(url);
         const data = await res.json();
 
         console.log("data", data);
-        
+
         setMovie(data);
         console.log("movie", movie.genres);
     }
@@ -36,28 +37,35 @@ export default function Movie(){
         getMovie(movieUrl)
     }, [])
 
-    return(
+    return (
         <div className="movie-page">
             {movie && (
-                <>
-                    <MovieCard movie={movie} showLink={false} />
-                    <p className="tagline">{movie.tagline}</p>
-                    <div className="info">
-                        <h3><BsWallet2 /> Orçamento:</h3>
-                        <p>{formatCurrency(movie.budget)}</p>
-                    </div>
-                    <div className="info">
-                        <h3><BsGraphUp /> Receita:</h3>
-                        <p>{formatCurrency(movie.revenue)}</p>
-                    </div>
-                    <div className="info">
-                        <p> {movie.runtime} Min</p>
-                    </div>
-                    <div className="info description">
-                        <h3>Sinopse:</h3>
-                        <p>{movie.overview}</p>
-                    </div>
-                </>
+                <div className="movie-description">
+                    <section className="lado-esquerdo">
+                        <MovieCard movie={movie} showLink={false} showMovieTitle={false} showMovieVoteAverage={false} />
+                    </section>
+                    <section className="lado-direito">
+                        <div className="info">
+                            <h2 className="movieTitle">{movie.title} <FaStar /> {movie.vote_average.toFixed(2)} / 10</h2>
+                            <p className="tagline">{movie.tagline}</p>
+                        </div>
+                        <div className="info">
+                            <h3><BsWallet2 /> Orçamento:</h3>
+                            <p>{formatCurrency(movie.budget)}</p>
+                        </div>
+                        <div className="info">
+                            <h3><BsGraphUp /> Receita:</h3>
+                            <p>{formatCurrency(movie.revenue)}</p>
+                        </div>
+                        <div className="info">
+                            <p> {movie.runtime} Min</p>
+                        </div>
+                        <div className="info description">
+                            <h3>Sinopse:</h3>
+                            <p>{movie.overview}</p>
+                        </div>
+                    </section>
+                </div>
             )}
         </div>
     )
